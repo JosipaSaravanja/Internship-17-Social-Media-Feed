263be8a0b70ba8576495b135f9bb2578f1da7b77
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-03-29 21:42:23 +0200

    added search

--- social-media/src/pages/FeedPage/FeedPage.jsx
+++ social-media/src/pages/FeedPage/FeedPage.jsx
@@ -7,9 +7,22 @@ import { useEffect, useState } from 'react';
 
 export const FeedPage=()=>{
     const [searchParams, setSearchParams] = useSearchParams();
-    const [showPosts, setShowPosts] = useState(posts);
+    const [searchPosts, setSearchPosts] = useState(posts);
+    useEffect(() => {
+      if (searchParams.get('search') === null) {
+        setSearchPosts(posts);
+      }
+      else {
+        setSearchPosts(posts.filter(
+              (post) => {
+                const user=users.find(el=>el.id==post.userId);
+                  return (post.text.toLowerCase().includes(searchParams.get('search').toLowerCase()) || user.username.toLowerCase().includes(searchParams.get('search').toLowerCase()))
+              }
+          ))
+      }
+  }, [searchParams])
     return <div>
-        {showPosts.map(product=>{
+        {searchPosts.map(product=>{
             return <Post post={product} user={users.find(user=>user.id===product.userId)}/>
         })}
     </div>
